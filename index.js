6390123343aad1bdbf9cd7c7d8d3610067b019a0
Mateus Prates <mateusbprates@hotmail.com>
2023-10-04 22:22:42 -0300

    Reestruturação do código

--- index.js
+++ index.js
@@ -1,58 +1,12 @@
-const dados = require('./variaveis');
-const express = require('express');
+require('dotenv').config();
+
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-const port = 3000;
 
-const client = require('twilio')(dados.accountSid, dados.authToken);
+//for user routes
+const userRoute = require("./routes/userRoute");
+app.use('/',userRoute);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.listen(3000,function(){
+    console.log("Server is running");
 });
-
-app.get('/reply', (req, res) => {
-    res.send('Hello World from reply');
-});
-
-
-app.post('/reply', express.json(), (req,res) => {
-    return new Promise((resolve, reject)=> {
-    console.log(req.body.Body);
-    
-    var messageToSend = "";
-
-    if(req.body.Body == 'hi')
-    {
-        messageToSend = "Hello There, How I can assist you"
-
-    }
-    else
-    {
-        messageToSend = "Hello" +req.body.Body+ "How are you! Let me know I can assist you";
-    }
-
-    client.messages
-        .create({
-            from: 'whatsapp:+14155238886',
-            body: 'Hello there!',
-            to: 'whatsapp:'+ dados.tel
-        })
-        .then(message => console.log(message.sid));
-
-    });
-
-    res.send('send via callback');
-
-});
-
-app.post('/callback', (red,res) => {
-    res.send('Hello Word!');
-});
-
-app.listen(port, () =>{
-    console.log('Example app listening at http://localhost:'+ port)
-})
-
-
-
