020f7b5e01a1704bf59ef4f32577bd2ce624741d
Mateus Prates <mateusbprates@hotmail.com>
2023-10-11 21:47:28 -0300

    Recebendo e enviando mensagem

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,43 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
+
 const twilio = require('twilio');
 
 const client = twilio(accountSid, authToken);
+
 
 const sendMessage = async(req,res)=>{
 
     try {
 
-        client.messages.create({
-            body: req.body.message,
-            from: 'whatsapp:+14155238886',
-            to: 'whatsapp:'+req.body.to
-        })
-        .then(message => console.log("Message sent successfully"));
+        // client.messages.create({
+        //     body: req.body.message,
+        //     from: 'whatsapp:+14155238886',
+        //     to: 'whatsapp:'+req.body.to
+        // })
+        // .then(message => console.log("Message sent successfully"));
 
-        return res.status(200).json({ success: true,msg:'Message sent successfully' });
+        console.log(req.body.Body);
+
+        var messageToSend = " ";
+
+        if(req.body.Body == "Tá ocupado?")
+        {
+            messageToSend = "Tô nada, fala aí"    
+        }
+
+        client.messages
+            .create({
+                from: 'whatsapp:+14155238886',
+                body: messageToSend,
+                to: req.body.From
+            })
+            .then(message => console.log("Message SID: " +message.sid));
+
+        res.send('send via callback');
+
+
     } catch (error) {
         return res.status(400).json({ success: false,msg:error.message });
     }
